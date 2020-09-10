8213bf3d40e3d4dbb9862eb4cc6e42198c1fe4af
BlakeDavis28 <blake_davis@outlook.com>
2020-09-11 08:57:27 +1000

    added geolocation functionality

--- src/index.js
+++ src/index.js
@@ -1,38 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = { lat: null, errorMessage: '' };
-
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
-  }
+state = {lat: null, errorMessage: ''};
 
 componentDidMount() {
-  console.log('my component was rendered to le screen');
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => this.setState({ lat: position.coords.latitude }),
+    (err) => this.setState({ errorMessage: err.message })
+  );
 }
 
-componentDidUpdate() {
-  console.log('my component was updated - it rerendered');
-}
-  
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>
